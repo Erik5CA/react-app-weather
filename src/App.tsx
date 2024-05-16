60b63059944957f7dbffb71f132d4481e797dba3
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-15 18:45:18 -0600

    updated component app, added Hourly and Daily components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import "./App.css";
+import Daily from "./components/Daily";
 import Details from "./components/Details";
 import Hourly from "./components/Hourly";
 
 import LocationDate from "./components/LocationDate";
 import ShowTemperture from "./components/ShowTemperture";
 import { useWeather } from "./hooks/useWeather";
+import { Weather } from "./types";
+import { formatWeekList } from "./utils/formatWeekWeather";
 
 function App() {
-  const weather = useWeather();
+  // const [formatedWeekList, setFormatedWeekList] = useState<Weather[][]>();
+
+  const weather = useWeather(false) as Weather;
+  const weaterWeekle = useWeather(true) as Weather[];
+  const formatedWeekList = formatWeekList(weaterWeekle);
+  // if (weaterWeekle) {
+  // setFormatedWeekList(formatWeekList(weaterWeekle));
+  // console.log(formatedWeekList[0]);
+  // }
 
   return (
     <>
       <LocationDate name={weather?.name} />
       {weather && <ShowTemperture {...weather} />}
-      <Hourly />
+      {formatedWeekList && <Hourly hourlyWeather={formatedWeekList[0]} />}
+      {formatedWeekList && <Daily dailyList={formatedWeekList} />}
       <Details
         humidity={weather?.main.humidity}
         pressure={weather?.main.pressure}
