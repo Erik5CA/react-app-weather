b4a3125b904d13326d01301c57346da88e0542c4
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-15 18:50:04 -0600

    updated component

--- src/components/LocationDate.tsx
+++ src/components/LocationDate.tsx
@@ -1,18 +1,6 @@
 import { LocationIcon } from "./Icons";
 import "../styles/LocationDate.css";
-
-//Format of the date => Tue, May 14 3:00 PM
-const formatDate = (date: Date) => {
-  const dateString = date.toUTCString();
-  const dateDM = dateString.split(" ").splice(0, 3).join(" ");
-  // Transform hour to 24 hrs to 12 hrs PM or AM
-  const minutes = date.getMinutes();
-  const hour24 = date.getHours();
-  const hour12 = hour24 % 12 || 12;
-  const ampm = hour24 < 12 ? "AM" : "PM";
-  const hour = `${hour12}:${minutes} ${ampm}`;
-  return `${dateDM} ${hour}`;
-};
+import { formatDate } from "../utils/formatDate";
 
 type Props = {
   name: string | undefined;
@@ -20,8 +8,6 @@ type Props = {
 
 function LocationDate({ name }: Props) {
   const date = new Date();
-  //   console.log(date);
-  formatDate(date);
   return (
     <div className="location">
       <LocationIcon />
