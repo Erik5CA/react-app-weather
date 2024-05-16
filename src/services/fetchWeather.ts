849ea241afd09e5c5f7cdd313f52547e03bb7bd7
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-15 18:52:27 -0600

    updated fetch, fetch current weather or list of 5 days

--- src/services/fetchWeather.ts
+++ src/services/fetchWeather.ts
@@ -1,11 +1,23 @@
 import { Location, Weather } from "../types.d";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
-const fetchWeather = async (location: Location): Promise<Weather> => {
+const fetchWeather = async (
+  location: Location,
+  isWeekly = false
+): Promise<Weather | Weather[]> => {
+  const parameter = isWeekly ? "forecast" : "weather";
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitud}&lon=${location.longitud}&appid=${API_KEY}&units=metric`
+    `${BASE_URL}${parameter}?lat=${location.latitud}&lon=${location.longitud}&appid=${API_KEY}&units=metric`
   );
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  if (isWeekly) {
+    const data = await response.json();
+    return data.list as Weather[];
+  }
   const data: Weather = await response.json();
   return data;
   // console.log(data);
