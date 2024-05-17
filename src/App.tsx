4150234dac6a25d1e25c12599efda68978624037
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-17 15:27:44 -0600

    update: document code, handle errors and conditional render

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,42 @@ import Hourly from "./components/Hourly";
 import LocationDate from "./components/LocationDate";
 import ShowTemperture from "./components/ShowTemperture";
 import { useWeather } from "./hooks/useWeather";
-import { useWeatherWeek } from "./hooks/useWeatherWeek";
 import { formatWeekList } from "./utils/formatWeekWeather";
-import { RefreshIcon } from "./components/Icons";
+import {
+  ErrorLocationIcon,
+  ErrorWeatherIcon,
+  RefreshIcon,
+} from "./components/Icons";
 
+/**
+ * The main component of the weather application.
+ * It handles the rendering of different sections based on the weather data and loading state.
+ * It also includes the scroll to top functionality.
+ */
 function App() {
-  const { weather, setUpdateWeather, loading } = useWeather();
-  const { weatherWeek } = useWeatherWeek();
+  /**
+   * Deconstructed object from useWeather hook.
+   * Contains weather, setUpdateWeather, loading, weatherWeek, and error.
+   */
+  const { weather, setUpdateWeather, loading, weatherWeek, error } =
+    useWeather();
+
+  /**
+   * Formatted week list of weather data.
+   * Created by passing weatherWeek to formatWeekList function.
+   */
   const formatedWeekList = formatWeekList(weatherWeek);
+
+  /**
+   * Ref object for the main div element.
+   * Used for scrolling to top.
+   */
   const ref = useRef<HTMLDivElement>(null);
 
+  /**
+   * Function to scroll to the top of the main div element.
+   * Uses the scroll method of the ref.current element.
+   */
   const scrollTop = () => {
     ref.current?.scroll({
       top: 0,
@@ -27,32 +53,48 @@ function App() {
 
   return (
     <div className="app" ref={ref}>
-      <LocationDate name={weather?.name} />
-      {weather ? (
-        <div className={loading ? "main" : "main main__animation"} id="main">
-          <ShowTemperture {...weather} />
-          {formatedWeekList && <Hourly hourlyWeather={formatedWeekList[0]} />}
-          {formatedWeekList && <Daily dailyList={formatedWeekList} />}
-          <Details
-            humidity={weather?.main.humidity}
-            pressure={weather?.main.pressure}
-            wind={weather?.wind.speed}
-            clouds={weather?.clouds.all}
-          />
-        </div>
-      ) : (
-        <div>
-          <div className="main">
-            <RefreshIcon cls="main__icon-loading" />
+      {!loading && !error && weather ? (
+        <>
+          <LocationDate name={weather?.name} />
+          <div className={loading ? "main" : "main main__animation"} id="main">
+            <ShowTemperture {...weather} />
+            <Hourly hourlyWeather={formatedWeekList[0]} />
+            <Daily dailyList={formatedWeekList} />
+            <Details
+              humidity={weather?.main.humidity}
+              pressure={weather?.main.pressure}
+              wind={weather?.wind.speed}
+              clouds={weather?.clouds.all}
+            />
           </div>
-          {/* <Footer setUpdateWeather={setUpdateWeather} loading={loading} / */}
+          <Footer
+            setUpdateWeather={setUpdateWeather}
+            loading={loading}
+            scrollTop={scrollTop}
+          />
+        </>
+      ) : (
+        <div className="main main--heigth">
+          <RefreshIcon cls="main__icon-loading" />
         </div>
       )}
-      <Footer
-        setUpdateWeather={setUpdateWeather}
-        loading={loading}
-        scrollTop={scrollTop}
-      />
+
+      {loading && !error && !weather && (
+        <div className="main main--heigth">
+          <RefreshIcon cls="main__icon-loading" />
+        </div>
+      )}
+      {error && !loading && (
+        <div className="main main--heigth">
+          {error.message.includes("location") ? (
+            <ErrorLocationIcon />
+          ) : (
+            <ErrorWeatherIcon />
+          )}
+
+          <h2 className="main__text_error">{error.message}</h2>
+        </div>
+      )}
     </div>
   );
 }
