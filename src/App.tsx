831f251294fa0e05ce4629e93a7594507478aeaa
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-16 18:21:52 -0600

    added footer and states for loading and update the weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,59 @@
+import { useRef } from "react";
 import "./App.css";
 import Daily from "./components/Daily";
 import Details from "./components/Details";
+import Footer from "./components/Footer";
 import Hourly from "./components/Hourly";
 
 import LocationDate from "./components/LocationDate";
 import ShowTemperture from "./components/ShowTemperture";
 import { useWeather } from "./hooks/useWeather";
-import { Weather } from "./types";
+import { useWeatherWeek } from "./hooks/useWeatherWeek";
 import { formatWeekList } from "./utils/formatWeekWeather";
+import { RefreshIcon } from "./components/Icons";
 
 function App() {
-  // const [formatedWeekList, setFormatedWeekList] = useState<Weather[][]>();
+  const { weather, setUpdateWeather, loading } = useWeather();
+  const { weatherWeek } = useWeatherWeek();
+  const formatedWeekList = formatWeekList(weatherWeek);
+  const ref = useRef<HTMLDivElement>(null);
 
-  const weather = useWeather(false) as Weather;
-  const weaterWeekle = useWeather(true) as Weather[];
-  const formatedWeekList = formatWeekList(weaterWeekle);
-  // if (weaterWeekle) {
-  // setFormatedWeekList(formatWeekList(weaterWeekle));
-  // console.log(formatedWeekList[0]);
-  // }
+  const scrollTop = () => {
+    ref.current?.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <>
+    <div className="app" ref={ref}>
       <LocationDate name={weather?.name} />
-      {weather && <ShowTemperture {...weather} />}
-      {formatedWeekList && <Hourly hourlyWeather={formatedWeekList[0]} />}
-      {formatedWeekList && <Daily dailyList={formatedWeekList} />}
-      <Details
-        humidity={weather?.main.humidity}
-        pressure={weather?.main.pressure}
-        wind={weather?.wind.speed}
-        clouds={weather?.clouds.all}
+      {weather ? (
+        <div className={loading ? "main" : "main main__animation"} id="main">
+          <ShowTemperture {...weather} />
+          {formatedWeekList && <Hourly hourlyWeather={formatedWeekList[0]} />}
+          {formatedWeekList && <Daily dailyList={formatedWeekList} />}
+          <Details
+            humidity={weather?.main.humidity}
+            pressure={weather?.main.pressure}
+            wind={weather?.wind.speed}
+            clouds={weather?.clouds.all}
+          />
+        </div>
+      ) : (
+        <div>
+          <div className="main">
+            <RefreshIcon cls="main__icon-loading" />
+          </div>
+          {/* <Footer setUpdateWeather={setUpdateWeather} loading={loading} / */}
+        </div>
+      )}
+      <Footer
+        setUpdateWeather={setUpdateWeather}
+        loading={loading}
+        scrollTop={scrollTop}
       />
-    </>
+    </div>
   );
 }
 
