b24518e8eafdd62dd2e48869d61b9d006c999fe9
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-17 15:32:18 -0600

    updated

--- src/hooks/useWeatherWeek.tsx
+++ src/hooks/useWeatherWeek.tsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
 import { Weather } from "../types.d";
 import { fetchWeatherWeek } from "../services/fetchWeather";
+import { getCurrentLocation } from "../services/geolocation";
 
 export const useWeatherWeek = () => {
   const [weatherWeek, setWeatherWeek] = useState<Weather[]>();
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const newLocalitacion = {
-        latitud: position.coords.latitude,
-        longitud: position.coords.longitude,
-      };
-      const getWeatherWeek = async () => {
+    const getWeatherWeek = async () => {
+      const { latitude, longitude, isErro } = await getCurrentLocation();
+      // console.log({ latitude, longitude });
+      if (isErro) {
+        throw new Error("We couldn´t get your location.");
+      } else {
+        const newLocalitacion = {
+          latitude,
+          longitude,
+        };
         const newWeather = await fetchWeatherWeek(newLocalitacion);
         setWeatherWeek(newWeather);
-      };
-      getWeatherWeek();
-    });
+      }
+    };
+    getWeatherWeek();
   }, []);
 
   return { weatherWeek };
